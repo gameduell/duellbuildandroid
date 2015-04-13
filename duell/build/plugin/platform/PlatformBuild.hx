25cb52153990eba8c3a25e6868b7bfcbabb025d4
Rui Campos <rui.campos@gameduell.de>
2015-04-13 18:50:23 +0200

    forgot to change setup android version

--- duell/build/plugin/platform/PlatformBuild.hx
+++ duell/build/plugin/platform/PlatformBuild.hx
@@ -33,7 +33,7 @@ using StringTools;
 
 class PlatformBuild
 {
-	public var requiredSetups = [{name: "android", version: "2.0.0"}];
+	public var requiredSetups = [{name: "android", version: "3.0.0"}];
 	public var supportedHostPlatforms = [LINUX, WINDOWS, MAC];
 	private static inline var TEST_RESULT_FILENAME = "test_result_android.xml";
 	private static inline var DEFAULT_ARMV7_EMULATOR = "duellarmv7";
